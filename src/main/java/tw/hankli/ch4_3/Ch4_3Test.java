4ce4b04085bd2e9cc3b6d1473c1453e2b63c764f
Hank Li <hankli0130@gmail.com>
2018-09-18 12:19:46 +0800

    ch4-3

---
+++ src/main/java/tw/hankli/ch4_3/Ch4_3Test.java
@@ -0,0 +1,44 @@
+package tw.hankli.ch4_3;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class Ch4_3Test {
+
+    public static void main(String[] args) {
+        fromThrow().subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                System.out.println("onNext " + aLong);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.err.println("onError " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
+    }
+
+    public static Observable<Long> fromEmpty() {
+        return Observable.empty();
+    }
+
+    public static Observable<Long> fromNever() {
+        return Observable.never();
+    }
+
+    public static Observable<Long> fromThrow() {
+        return Observable.error(new Exception("exception!!"));
+    }
+}
